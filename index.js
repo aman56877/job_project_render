707e898d33328d9ff5c492856d000b6e2f56aa23
aman56877 <guptakabir5687@gmail.com>
2024-02-16 21:13:59 +0530

    cors added

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ require('dotenv').config();
 require("./dbConfig");
 const express = require('express')
 const app = express()
+const cors = require("cors")
 const port = process.env.PORT
 const getRoute = require("./routes/getRoutes");
 const postRoute = require("./routes/postRoutes");
 
 
 app.use(express.json());
-
+app.use(cors());
 app.use("/", getRoute);
 app.use("/", postRoute);
 
